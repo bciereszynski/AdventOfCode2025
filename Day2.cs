591c54a424ed86eb689125a7f7cda6554669d92a
bciereszynski <ciereszynskibp@gmail.com>
2025-12-03 00:41:48 +0100

    D2: improve code performence and readability

--- Day2.cs
+++ Day2.cs
@@ -20,38 +20,79 @@ bool isInvalid1(long value)
     return false;
 }
 
-
-bool isInvalid2(long value)
+static bool[] SieveOfEratosthenes(int size)
 {
-    var size = value.ToString().Length;
-
-    for (int number = 2; number <= size; ++number)
+    if (size < 2)
     {
-        long tmpVal = value;
-        if (size % number != 0)
+        return new bool[0];
+    }
+
+    bool[] isPrime = new bool[size + 1];
+    for (int i = 0; i <= size; i++)
+    {
+        isPrime[i] = true;
+    }
+
+    isPrime[0] = false;
+    isPrime[1] = false;
+
+    for (int p = 2; p * p <= size; p++)
+    {
+        if (isPrime[p])
         {
-            continue;    
-        }
-        var division = Convert.ToInt64(Math.Pow(10, size / number));
-        var previous = tmpVal % division;
-        tmpVal /= division;
-        for (long i = size / number; i <= size; i += size / number)
-        {
-            if (i == size)
+            for (int i = p * p; i <= size; i += p)
             {
-                return true;
+                isPrime[i] = false;
             }
-            var curr = tmpVal % division;
-            tmpVal /= division;
-            if (curr != previous)
-            {
-                break;
-            }
-            previous = curr;
         }
     }
 
+    return isPrime;
+}
 
+
+
+bool isInvalid2(long value, bool[] isPrime)
+{
+    long getBatch(ref long value, long division)
+    {
+        var batch = value % division;
+        value /= division;
+        return batch;
+    }
+
+    int numberOfDigits = value != 0 ? (int)Math.Floor(Math.Log10(Math.Abs(value))) + 1 : 1;
+
+    for (int batchCount = 2; batchCount <= numberOfDigits; ++batchCount)
+    {
+        long localValue = value;
+        if (numberOfDigits % batchCount != 0)
+        {
+            continue;
+        }
+        if (!isPrime[batchCount])
+        {
+            continue;
+        }
+        int batchSize = numberOfDigits / batchCount;
+        long division = Convert.ToInt64(Math.Pow(10, batchSize));
+        long previousBatch = getBatch(ref localValue, division);
+        for (int processedCharacters = batchSize; processedCharacters <= numberOfDigits; processedCharacters += batchSize)
+        {
+            if (processedCharacters == numberOfDigits)
+            {
+                return true;
+            }
+
+            long currBatch = getBatch(ref localValue, division);
+
+            if (currBatch != previousBatch)
+            {
+                break;
+            }
+            previousBatch = currBatch;
+        }
+    }
 
     return false;
 }
@@ -66,9 +107,11 @@ foreach (string range in line.Split(','))
     var begin = long.Parse(range.Substring(0, separator));
     var end = long.Parse(range.Substring(separator + 1));
 
+    bool[] isPrime = SieveOfEratosthenes(1000);
+
     for (var i = begin; i <= end; i++)
     {
-        if (isInvalid2(i))
+        if (isInvalid2(i, isPrime))
         {
             count += i;
         }
