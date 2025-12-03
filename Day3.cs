19634d0a2d25d07545c6b0fecd5fc3bd95677551
bciereszynski <ciereszynskibp@gmail.com>
2025-12-04 00:37:17 +0100

    D3: multi batteries solution

--- Day3.cs
+++ Day3.cs
@@ -1,29 +1,31 @@
 using System;
 
 const string fileName = @"inputs\day3.txt";
-int sum = 0;
+long sum = 0;
+List<(int idx, int value)> pairs;
 
 foreach (string line in File.ReadLines(fileName))
 {   
-    int first = 0;
-    int second = 0;
+    pairs = new List<(int idx, int value)>();
+    int itemCount = 12;
+    int idx = 0;
+    int lineSize = line.Length;
     foreach (char c in line)
     {
-        int curr = int.Parse(c.ToString());
-        if(curr > first)
-        {
-            second = -first;
-            first = curr;
-        }
-        else if(curr > second)
-        {
-            second = curr;
-        }
+        pairs.Add((idx, c - '0'));
+        idx++;
     }
-    if(second < 0)
+
+    pairs = pairs.OrderByDescending(p => p.value).ToList();
+    
+    long joltage = 0;
+    for (int i = itemCount; i >0; i--)
     {
-        (first, second) = (-second, first);
+       var biggestPossibleElement = pairs.FirstOrDefault(p => lineSize - p.idx >= i);
+       joltage = joltage *10 + biggestPossibleElement.value;
+       pairs.RemoveAll(p => p.idx <= biggestPossibleElement.idx);
     }
-    sum += first * 10 + second;
+    sum += joltage;
+  
 }    
 Console.WriteLine(sum + "");
