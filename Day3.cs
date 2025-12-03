77794749cebc17c1d936cf91f9ae0c28ef10eb1a
bciereszynski <ciereszynskibp@gmail.com>
2025-12-03 23:44:36 +0100

    D3: two batteries solution

--- Day3.cs
+++ Day3.cs
@@ -1,14 +1,29 @@
 using System;
 
-const string fileName = @"inputs\day3_small.txt";
+const string fileName = @"inputs\day3.txt";
+int sum = 0;
 
 foreach (string line in File.ReadLines(fileName))
 {   
-
-   foreach (char c in line)
-   {
-       int curr = int.Parse(c.ToString());
-       Console.Write(curr + " ");
-   }
-   Console.WriteLine("");
-}
+    int first = 0;
+    int second = 0;
+    foreach (char c in line)
+    {
+        int curr = int.Parse(c.ToString());
+        if(curr > first)
+        {
+            second = -first;
+            first = curr;
+        }
+        else if(curr > second)
+        {
+            second = curr;
+        }
+    }
+    if(second < 0)
+    {
+        (first, second) = (-second, first);
+    }
+    sum += first * 10 + second;
+}    
+Console.WriteLine(sum + "");
