0951f0cfefe7b1b67a84fac3abb56fe9fa527328
bciereszynski <ciereszynskibp@gmail.com>
2025-12-06 22:12:48 +0100

    D5: part two - all indexes

--- Day5.cs
+++ Day5.cs
@@ -10,6 +10,7 @@ var rangesLines = lines.Take(splitIndex).ToList();
 var ingridientsLines = lines.Skip(splitIndex + 1).ToList();
 
 List<(long start, long end)> ranges = new List<(long start, long end)>();
+long freshCount = 0;
 
 foreach (var line in rangesLines)
 {
@@ -17,22 +18,73 @@ foreach (var line in rangesLines)
     var start = long.Parse(parseLine[0]);
     var end = long.Parse(parseLine[1]);
 
-    ranges.Add((start, end));
-}
-
-var freshCount = 0;
-foreach (var line in ingridientsLines)
-{
-    var ingridient = long.Parse(line);
-    for (var i = 0; i < ranges.Count; i++)
+    (long start, long end)? startRange = null, endRange = null;
+    foreach (var range in ranges)
     {
-        var range = ranges[i];
-        if (ingridient >= range.start && ingridient <= range.end)
+        if (start >= range.start && start <= range.end)
         {
-            freshCount++;
-            break;
+            startRange = range;
         }
+        if (end >= range.start && end <= range.end)
+        {
+            endRange = range;
+        }
+    }
+
+    if (startRange == null && endRange == null)
+    {
+        freshCount += AddRange(ranges, start, end);
+        continue;
+    }
+    if (startRange != null && endRange != null)
+    {
+        if (startRange == endRange)
+        {
+            continue;
+        }
+
+        var mergedRange = (startRange.Value.start, endRange.Value.end);
+        freshCount += AddRange(ranges, mergedRange.start, mergedRange.end);
+        continue;
+
+    }
+    if (startRange != null)
+    {
+        var extendedRange = (startRange.Value.start, end);
+        freshCount += AddRange(ranges, extendedRange.start, extendedRange.end);
+        continue;
+    }
+
+    if (endRange != null)
+    {
+        var extendedRange = (start, endRange.Value.end);
+        freshCount += AddRange(ranges, extendedRange.start, extendedRange.end);
+        continue;
     }
 }
 
 Console.WriteLine(freshCount);
+
+
+static long AddRange(List<(long start, long end)> ranges, long start, long end)
+{
+
+    long countChange = end - start + 1;
+    var rangesToRemove = ranges.Where(range => range.start >= start && range.start <= end || range.end >= start && range.end <= end).ToList();
+    foreach (var r in rangesToRemove)
+    {
+        countChange -= r.end - r.start + 1;
+        ranges.Remove(r);
+    }
+    ranges.Add((start, end));
+    return countChange;
+}
+
+static void PrintRanges(List<(long start, long end)> ranges)
+{
+    Console.WriteLine("---");
+    foreach (var range in ranges)
+    {
+        Console.WriteLine($"{range.start}-{range.end}");
+    }
+}
