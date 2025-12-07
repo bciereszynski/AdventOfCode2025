32a99516031008cc60f443ecb0713df4396624ed
bciereszynski <ciereszynskibp@gmail.com>
2025-12-07 14:19:22 +0100

    D6: part two - column numbers

--- Day6.cs
+++ Day6.cs
@@ -1,40 +1,60 @@
 using System;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 var lines = File.ReadLines(@"./inputs/day6.txt").ToList();
 
-var numberLines = lines.Take(lines.Count - 1).ToList();
-List<string> operandsArray = Regex.Replace(lines.Last().Trim(), "\\s+", " ").Split(" ").ToList();
+var operandsLine = lines.Last();
 
-var numbersGrid = new int[numberLines.Count, operandsArray.Count];
+List<string> operandsArray = Regex.Replace(operandsLine.Trim(), "\\s+", " ").Split(" ").ToList();
 
-foreach (var line in numberLines.Select((value, index) => new { value, index }))
+var numbersGrid = new List<int>[operandsArray.Count];
+string[] numberLines = lines.Take(lines.Count - 1).ToArray();
+var currEquationNumber = -1;
+
+for (int i = 0; i < numberLines[0].Length; i++)
 {
-    var trimmedLine = Regex.Replace(line.value.Trim(), "\\s+", " ");
-    foreach (var numeberString in trimmedLine.Split(" ").Select((value, index) => new { value, index }))
+    var number = 0;
+
+    for (int j = 0; j < numberLines.Length; j++)
     {
-        var number = int.Parse(numeberString.value);
-        numbersGrid[line.index, numeberString.index] = number;
+        var currChar = numberLines[j][i];
+        if (currChar == ' ')
+        {
+            continue;
+        }
+        var digit = Convert.ToInt32(currChar - '0');
+        number = number * 10 + digit;
+
     }
+    if (number == 0)
+    {
+        continue;
+    }
+    if (operandsLine.Length > i && operandsLine[i] != ' ')
+    {
+        currEquationNumber++;
+        numbersGrid[currEquationNumber] = new List<int>();
+    }
+    numbersGrid[currEquationNumber].Add(number);
 }
 
 long result = 0;
 for (int i = 0; i < operandsArray.Count; i++)
 {
     long localResult;
-    var column = Enumerable.Range(0, numbersGrid.GetLength(0))
-                                       .Select(j => numbersGrid[j, i]);
     switch (operandsArray[i])
     {
         case "+":
-            localResult = column.Sum();
+
+            localResult = numbersGrid[i].Sum();
             break;
 
         case "*":
-            localResult = column.Aggregate(1L, (acc, x) => acc * x);
+            localResult = numbersGrid[i].Aggregate(1L, (acc, x) => acc * x);
             break;
         default:
-            throw new Exception("corrupted file");
+            throw new Exception("corrupted file:" + operandsArray[i]);
     }
     result += localResult;
 }
