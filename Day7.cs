8424d2a1403ada0532d617f9b6ed05faf650f20b
bciereszynski <ciereszynskibp@gmail.com>
2025-12-07 17:25:04 +0100

    D7: part one - simple beem

---
+++ Day7.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+var lines = File.ReadLines(@"inputs/day7.txt");
+
+var beemState = new char[lines.First().Length];
+
+var splitCount = 0;
+
+foreach (var line in lines)
+{
+
+    for (int idx = 0; idx < line.Length; idx++)
+    {
+        var value = line[idx];
+        switch (value)
+        {
+            case '.':
+                beemState[idx] = beemState[idx];
+                break;
+            case 'S':
+                beemState[idx] = '|';
+                break;
+            case '^':
+                if (beemState[idx] == '|')
+                {
+                    beemState[idx - 1] = '|';
+                    beemState[idx] = ' ';
+                    beemState[++idx] = '|';
+                    splitCount++;
+                }
+                break;
+            default:
+                throw new Exception("bad input");
+        }
+    }
+}
+
+Console.WriteLine(splitCount);
